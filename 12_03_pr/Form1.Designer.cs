8c7d3c60174680cf42e4eefe1cb423738cae3c10
Dmytro Bondarenko <Dmytro.Bondarenko@cs.khpi.edu.ua>
2024-03-17 19:00:02 +0200

    v2_0

--- 12_03_pr/Form1.Designer.cs
+++ 12_03_pr/Form1.Designer.cs
@@ -34,21 +34,21 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button4 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
             this.button10 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 24);
+            this.button1.Location = new System.Drawing.Point(460, 72);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(288, 30);
+            this.button1.Size = new System.Drawing.Size(190, 57);
             this.button1.TabIndex = 0;
             this.button1.Text = "Перша кнопка вводить у колекцію цілі числа";
             this.button1.UseVisualStyleBackColor = true;
@@ -57,7 +57,7 @@
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(388, 53);
+            this.listBox1.Location = new System.Drawing.Point(694, 45);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(350, 147);
             this.listBox1.TabIndex = 1;
@@ -66,7 +66,7 @@
             // listBox2
             // 
             this.listBox2.FormattingEnabled = true;
-            this.listBox2.Location = new System.Drawing.Point(388, 241);
+            this.listBox2.Location = new System.Drawing.Point(694, 233);
             this.listBox2.Name = "listBox2";
             this.listBox2.Size = new System.Drawing.Size(350, 173);
             this.listBox2.TabIndex = 2;
@@ -74,9 +74,9 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 77);
+            this.button2.Location = new System.Drawing.Point(460, 146);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(288, 32);
+            this.button2.Size = new System.Drawing.Size(190, 53);
             this.button2.TabIndex = 3;
             this.button2.Text = "Друга кнопка цілими числами заповнює другу колекцію";
             this.button2.UseVisualStyleBackColor = true;
@@ -84,9 +84,9 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(12, 129);
+            this.button3.Location = new System.Drawing.Point(327, 233);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(288, 71);
+            this.button3.Size = new System.Drawing.Size(341, 71);
             this.button3.TabIndex = 4;
             this.button3.Text = "Третя кнопка видаляє елементи, які є у другій колекції з першої і показує оновлен" +
     "у першу колекцію.";
@@ -95,54 +95,33 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 241);
+            this.textBox1.Location = new System.Drawing.Point(319, 91);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 5;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(136, 241);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(228, 40);
-            this.button4.TabIndex = 6;
-            this.button4.Text = "і  кнопка заповнюють колекцію1 цілими числами.";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
-            // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(12, 313);
+            this.textBox2.Location = new System.Drawing.Point(319, 163);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 7;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(136, 303);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(228, 39);
-            this.button5.TabIndex = 8;
-            this.button5.Text = "і друга кнопка заповнюють колекцію2 цілими числами. ";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
-            // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(12, 378);
+            this.button6.Location = new System.Drawing.Point(12, 423);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(352, 49);
             this.button6.TabIndex = 9;
-            this.button6.Text = "Третя кнопка показує  у іншому списку частину першої колекції – ті елементи номер" +
-    "ами яких є числа з другої колекції";
+            this.button6.Text = "3";
             this.button6.UseVisualStyleBackColor = true;
             this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(21, 475);
+            this.button7.Location = new System.Drawing.Point(13, 499);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(343, 42);
             this.button7.TabIndex = 10;
@@ -152,7 +131,7 @@
             // 
             // button8
             // 
-            this.button8.Location = new System.Drawing.Point(21, 549);
+            this.button8.Location = new System.Drawing.Point(13, 573);
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(343, 34);
             this.button8.TabIndex = 11;
@@ -162,18 +141,17 @@
             // 
             // button9
             // 
-            this.button9.Location = new System.Drawing.Point(21, 625);
+            this.button9.Location = new System.Drawing.Point(327, 326);
             this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(343, 39);
+            this.button9.Size = new System.Drawing.Size(341, 80);
             this.button9.TabIndex = 12;
-            this.button9.Text = "Перша кнопка створює масив з 3ох випадкових цілих чисел  і додає його у колекцію." +
-    "";
+            this.button9.Text = "(Z2)фільтрації елементів колекції 1 за наявністю їхніх індексів у колекції 2";
             this.button9.UseVisualStyleBackColor = true;
             this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // button10
             // 
-            this.button10.Location = new System.Drawing.Point(21, 698);
+            this.button10.Location = new System.Drawing.Point(12, 644);
             this.button10.Name = "button10";
             this.button10.Size = new System.Drawing.Size(343, 48);
             this.button10.TabIndex = 13;
@@ -182,19 +160,39 @@
             this.button10.UseVisualStyleBackColor = true;
             this.button10.Click += new System.EventHandler(this.button10_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(570, 441);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(417, 449);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1069, 851);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button10);
             this.Controls.Add(this.button9);
             this.Controls.Add(this.button8);
             this.Controls.Add(this.button7);
             this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
             this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.button4);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -216,14 +214,14 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button4;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.Button button9;
         private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- 12_03_pr/Form1.cs
+++ 12_03_pr/Form1.cs
@@ -16,10 +16,23 @@ namespace _12_03_pr
         {
             InitializeComponent();
         }
+        List<int> collection1 = new List<int>(); // колекція 1
+        List<int> collection2 = new List<int>(); // колекція 2
+        List<Label> labelList = new List<Label>(); // список label
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            // Додає у колекцію цілі числа з першого текстового поля
+            string[] numbers = textBox1.Text.Split(' ');
+            foreach (string number in numbers)
+            {
+                if (int.TryParse(number, out int result))
+                {
+                    collection1.Add(result);
+                }
+            }
+            listBox1.Items.Clear();
+            listBox1.Items.AddRange(collection1.Select(x => x.ToString()).ToArray());
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -29,48 +42,92 @@ namespace _12_03_pr
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            // Додає у колекцію цілі числа з другого текстового поля
+            string[] numbers = textBox2.Text.Split(' ');
+            foreach (string number in numbers)
+            {
+                if (int.TryParse(number, out int result))
+                {
+                    collection2.Add(result);
+                }
+            }
+            listBox2.Items.Clear();
+            listBox2.Items.AddRange(collection2.Select(x => x.ToString()).ToArray());
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-
+            // Видаляє елементи, які є у колекції 2 з колекції 1
+            foreach (int number in collection2)
+            {
+                collection1.RemoveAll(x => x == number);
+            }
+            listBox1.Items.Clear();
+            listBox1.Items.AddRange(collection1.Select(x => x.ToString()).ToArray());
         }
-
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
 
         }
 
-        private void button4_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button5_Click(object sender, EventArgs e)
-        {
-
-        }
 
         private void button6_Click(object sender, EventArgs e)
         {
-
+            // Створюємо новий Label
+            Label newLabel = new Label();
+            // Налаштовуємо його властивості
+            newLabel.Text = "Новий Label"; // Замініть цей текст на те, що потрібно відображати
+            newLabel.AutoSize = true; // Автоматично налаштувати розмір Label
+                                      // Розміщаємо новий Label на формі
+            int labelTop = 50 + labelList.Count * 30; // Визначаємо вертикальне положення Label
+            newLabel.Location = new Point(50, labelTop); // Встановлюємо положення Label на формі
+            this.Controls.Add(newLabel); // Додаємо Label до колекції контролів форми
+                                         // Додаємо новий Label до колекції labelList
+            labelList.Add(newLabel);
         }
+
 
         private void button7_Click(object sender, EventArgs e)
         {
-
+           
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
-
+            // Знаходить і показує масив з цієї колекції з максимальною сумою елементів
+            List<int[]> arrays = new List<int[]>(); // колекція масивів
+            // Додавання масивів в колекцію
+            Random rnd = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                int[] randomArray = new int[3];
+                for (int j = 0; j < 3; j++)
+                {
+                    randomArray[j] = rnd.Next(-100, 100); // генеруємо випадкове число від -100 до 99
+                }
+                arrays.Add(randomArray);
+            }
+            // Пошук масиву з максимальною сумою елементів
+            int maxSum = arrays.Max(arr => arr.Sum());
+            int[] maxSumArray = arrays.First(arr => arr.Sum() == maxSum);
+            // Вивід масиву з максимальною сумою елементів
+            MessageBox.Show("Масив з максимальною сумою елементів: " + string.Join(", ", maxSumArray));
         }
-
         private void button9_Click(object sender, EventArgs e)
         {
-
+            listBox2.Items.Clear();
+            // Створюємо множину для зберігання унікальних значень індексів з колекції 2
+            HashSet<int> uniqueIndexes = new HashSet<int>(collection2);
+            // Сортуємо та фільтруємо індекси
+            List<int> sortedIndexes = uniqueIndexes.Where(index => index >= 0 && index < collection1.Count).OrderBy(index => index).ToList();
+            // Виводимо значення елементів за відфільтрованими індексами
+            foreach (int index in sortedIndexes)
+            {
+                listBox2.Items.Add(collection1[index]);
+            }
         }
+
+
 
         private void button10_Click(object sender, EventArgs e)
         {
@@ -85,6 +142,27 @@ namespace _12_03_pr
         private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // Беремо номер елементу з текстбоксу1
+            if (int.TryParse(textBox1.Text, out int index) && index >= 0 && index < labelList.Count)
+            {
+                // Беремо текст з текстбоксу2
+                string newText = textBox2.Text;
+                // Знаходимо Label за вказаним індексом та змінюємо його текст
+                labelList[index].Text = newText;
+            }
+            else
+            {
+                MessageBox.Show("Введіть коректний номер елементу.");
+            }
         }
     }
 }
